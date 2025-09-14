6d0ce7687003c009164411e67cec4ceb86335fa8
hexenmeister <maxwelltm@gmail.com>
2025-09-14 11:58:54 +0300

    my 4 lesson

---
+++ src/main/kotlin/com/stimmax/kotlincourse/lessons/lesson03/kotfile.kt
@@ -0,0 +1,51 @@
+package org.example.ru.stimmax.kotlincourse.lessons.lesson3
+
+val vincode: String = "1234"
+
+var color: String = "blue"
+var mileage: Int = 0
+lateinit var ownername: String
+val wheels: Int = 4
+val myint: Int = 5
+val myfloat: Float = 0.58F
+val mylong: Long = 1_23_233_890L //длинее инта во много раз но дольше
+val myshort: Short = 32000 // не нужны
+val mybyte: Byte = 120 // один байт до +-127
+val mydouble: Double = 5.99 // дробное число, если целое число то ноль в любом случае
+val iskotlinfun: Boolean = true
+val letter: Char = 's' //любой один символ в одиночной кавычке
+val text: String = "text" // любой текст в двойных кавычках
+val numbers: Array<Int> = arrayOf(1,2,3)
+val strings: List<String> = listOf("one", "two", "three")
+val doubles: Set<Double> = setOf(23.5, 22.9)
+val keystovalues: Map<Int, String> = mapOf( //словарь! полка с книгами
+    1 to "значение 1",
+    2 to "значение 2"
+)
+val numbers: Array<Any> = arrayOf(1,2,"sum")
+
+val s1: Byte = 100
+val s2: Long = 156L
+val s3: Float = 12.34f
+val s4: Double = 123.456
+val s5: String = "Hello, World!"
+val s6: Boolean = true
+val s7: Char = 'a'
+val s8: Byte = 0
+val s9: Long = 9999999999L
+val s10: Float = 25.0f
+val s11: Double = -123.4
+val s12: List<Int> = listOf(2, 3, 4)
+val s13: Char = '?'
+val s14: String = "Kotlin"
+val s15: Set<Double> = setOf(3.14, 45.0)
+val s16: Boolean = false
+val s17: String = "D"
+val s18: Long = 2147483648L
+val s19: Array<String> = arrayOf("Hello", "world")
+val s20: Boolean = "true"
+val s21: Map<Int, String> = mapOf(12 to "Kotlin")
+val s22: Float = 0.1f
+val s23: Char = '!'
+val s24: Map<String, Any?> = mapOf("a" to 'a', "b" to true, "c" to null)
+// вопросик после эни вообще что хочешь воткнуть можно
